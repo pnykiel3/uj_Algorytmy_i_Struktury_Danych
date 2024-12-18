1f07eea8f1ad12e31cf298d5a70ae67e8a1bbda5
Paweł Nykiel <p.nykiel@icloud.com>
2024-12-18 14:20:01 +0100

    finished

--- Zestaw11/binarysearchtree.h
+++ Zestaw11/binarysearchtree.h
@@ -1,8 +1,117 @@
-//
-// Created by pawelubuntu on 12/11/24.
-//
-
 #ifndef BINARYSEARCHTREE_H
 #define BINARYSEARCHTREE_H
+
+#include <iostream>
+#include <cassert>
+
+template <typename T>
+struct BSTNode {
+    T value;
+    BSTNode *left, *right;
+
+    BSTNode() : value(T()), left(nullptr), right(nullptr) {}
+    BSTNode(const T& item) : value(item), left(nullptr), right(nullptr) {}
+    ~BSTNode() {
+        delete left; delete right;
+    }
+};
+
+
+// Szablon dla binarnego drzewa poszukiwań.
+template <typename T>
+class BinarySearchTree {
+    BSTNode<T> *root;
+public:
+    BinarySearchTree() : root(nullptr) {} // konstruktor domyślny
+    ~BinarySearchTree() {
+        delete root;
+    } // trzeba wyczyścić
+    void clear() {
+        delete root; root = nullptr;
+    }
+    bool empty() const {
+        return root == nullptr;
+    }
+    T& top() {
+        assert(root != nullptr); return root->value;
+    } // podgląd korzenia
+    void insert(const T& item) {
+        root = insert(root, item);
+    }
+
+
+    // Szukając element dostajemy wskaźnik do elementu lub nullptr.
+    T* search(const T& item) const {
+        auto ptr = search(root, item);
+        return ((ptr == nullptr) ? nullptr : &(ptr->value));
+    }
+    T* iter_search(const T& item) const { // wg libavl
+        for (auto node=root; node != nullptr; ) {
+            if (item == node->value) {
+                return &(node->value);
+            } else if (item < node->value) {
+                node = node->left;
+            } else {
+                node = node->right;
+            }
+        }
+        return nullptr;
+    }
+
+
+    T* find_min() {
+        if (root == nullptr) { // jeśli drzewo jest puste to zwracamy nullptr
+            return nullptr;
+        }
+        BSTNode<T>* node = root;
+        while (node->left != nullptr) {
+            node = node->left;
+        }
+        return &(node->value); // zwracamy wskaźnik na najmniejsza wartość
+    }
+
+
+    T* find_max() {
+        if (root == nullptr) { // jeśli drzewo jest puste to zwracamy nullptr
+            return nullptr;
+        }
+        BSTNode<T>* node = root;
+        while (node->right != nullptr) {
+            node = node->right;
+        }
+        return &(node->value); // zwracamy wskaźnik na najwieksza wartość
+    }
+
+
+    void display() {
+        display(root, 0);
+    }
+
+
+    void display(BSTNode<T>* node, int level) { // ze strony kursu
+        if (node == nullptr) return;
+        display(node->right, level + 1);
+        for (int i = 0; i < level; i++) {
+            std::cout << "   |";
+        }
+        std::cout << "---" << node->value << std::endl;
+        display(node->left, level + 1);
+    }
+
+
+    BSTNode<T>* insert(BSTNode<T> *node, const T& item) {
+        if (node == nullptr) {
+            return new BSTNode<T>(item);
+        }
+        if (item < node->value) {
+            node->left = insert(node->left, item);
+        }
+        else {
+            node->right = insert(node->right, item);
+        }
+        return node; // zwraca nowy korzeń
+    }
+};
+
 
 #endif //BINARYSEARCHTREE_H
--- Zestaw11/main.cpp
+++ Zestaw11/main.cpp
@@ -1,3 +1,136 @@
-//
-// Created by pawelubuntu on 12/15/24.
-//
+#include "binarysearchtree.h"
+
+void test1() {
+    std::cout << "Test1:\t";
+    BinarySearchTree<int> tree;
+
+    tree.insert(14);
+    tree.insert(8);
+    tree.insert(2);
+    tree.insert(58);
+    tree.insert(16);
+    tree.insert(10);
+    tree.insert(47);
+    assert(tree.top() == 14);
+    assert(*tree.find_min() == 2);
+    assert(*tree.find_max() == 58);
+    tree.clear();
+    assert(tree.empty());
+    std::cout << "[[poprawny]]" << std::endl;
+}
+
+void test2() {
+    std::cout << "Test2:\t";
+    BinarySearchTree<int> tree;
+
+    tree.insert(60);
+    tree.insert(49);
+    tree.insert(47);
+    tree.insert(8);
+    tree.insert(16);
+    tree.insert(55);
+    tree.insert(69);
+    tree.insert(420);
+    tree.insert(220);
+    tree.insert(65);
+    tree.insert(777);
+    assert(*tree.find_min() == 8);
+    assert(*tree.find_max() == 777);
+    std::cout << "[[poprawny]]" << std::endl;
+}
+
+void test3() {
+    std::cout << "Test3:\t";
+    BinarySearchTree<int> tree;
+
+    tree.insert(5);
+    tree.insert(10);
+    tree.insert(15);
+    tree.insert(798);
+    tree.insert(12);
+    tree.insert(234);
+    tree.insert(3);
+    tree.insert(987);
+    tree.insert(1);
+    tree.insert(44);
+    assert(*tree.find_min() == 1);
+    assert(*tree.find_max() == 987);
+    std::cout << "[[poprawny]]" << std::endl;
+}
+
+void test4() {
+    std::cout << "Test4:\t";
+    BinarySearchTree<int> tree;
+
+    tree.insert(50);
+    tree.insert(10);
+    tree.insert(31);
+    tree.insert(34);
+    tree.insert(22);
+    tree.insert(7);
+    tree.insert(8);
+    tree.insert(1);
+    tree.insert(151);
+    tree.insert(69);
+    tree.insert(777);
+    tree.insert(90);
+    tree.insert(62);
+    tree.insert(502);
+    tree.insert(1001);
+    assert(*tree.find_min() == 1);
+    assert(*tree.find_max() == 1001);
+    std::cout << "[[poprawny]]" << std::endl;
+}
+
+void test5() {
+    std::cout << "Test5:\t";
+    BinarySearchTree<int> tree;
+
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+    tree.insert(4);
+    tree.insert(5);
+    tree.insert(6);
+    tree.insert(7);
+    tree.insert(8);
+    tree.insert(9);
+    tree.insert(10);
+    assert(*tree.find_min() == 1);
+    assert(*tree.find_max() == 10);
+    std::cout << "[[poprawny]]" << std::endl;
+}
+
+void test6() {
+    std::cout << "Test6:\t";
+    BinarySearchTree<int> tree;
+
+    tree.insert(61);
+    tree.insert(41);
+    tree.insert(73);
+    tree.insert(37);
+    tree.insert(13);
+    tree.insert(59);
+    tree.insert(19);
+    tree.insert(97);
+    tree.insert(83);
+    tree.insert(5);
+    assert(*tree.find_min() == 5);
+    assert(*tree.find_max() == 97);
+    std::cout << "[[poprawny]]" << std::endl;
+}
+
+int main() {
+    try {
+        test1();
+        test2();
+        test3();
+        test4();
+        test5();
+        test6();
+        std::cout << "Wszystkie testy zakończone pomyślnie!" << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Błąd testu: " << e.what() << std::endl;
+    }
+    return 0;
+}
